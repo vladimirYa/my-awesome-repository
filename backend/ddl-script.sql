0bd4137cc6eab97040e6f19b3e021a3ef4630c3d
Vlad Miliutin <vlad.milytin@gmail.com>
2017-11-12 21:21:34 +0200

    ddl script added

---
+++ backend/ddl-script.sql
@@ -0,0 +1,10 @@
+DROP schema PUBLIC;
+
+CREATE TABLE users(
+  user_id BIGSERIAL,
+  name VARCHAR(255) NOT NULL,
+  surname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+
+);
